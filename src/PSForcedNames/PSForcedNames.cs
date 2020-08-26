4a4f3cdc0e344ff2448909144ed59a18ed89767a
papershredder432 <34615274+papershredder432@users.noreply.github.com>
2020-08-26 13:24:11 -0400

    Initial Upload

---
+++ src/PSForcedNames/PSForcedNames.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Permissions;
+using SDG.Unturned;
+using Steamworks;
+
+namespace PSForcedNames
+{
+    public class PSForcedNames : RocketPlugin<PSForcedNamesConfiguration>
+    {
+        public PSForcedNames Instance;
+        
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.LogWarning("[PSForcedNames] Loaded, made by papershredder432, join the support Discord here: https://discord.gg/ydjYVJ2");
+
+            if (!Instance.Configuration.Instance.Enabled) UnloadPlugin();
+            
+            UnturnedPermissions.OnJoinRequested += OnJoinRequested;
+        }
+        
+        protected override void Unload()
+        {
+            Instance = null;
+            Logger.LogWarning("[PSForcedNames] Unloaded.");
+        }
+        
+        private void OnJoinRequested(CSteamID player, ref ESteamRejection? rejectionreason)
+        {
+            SteamPending steamPending =
+                Provider.pending.FirstOrDefault(x => x.playerID.steamID.m_SteamID == player.m_SteamID);
+
+            if (Instance.Configuration.Instance.Players.Exists(x =>
+                x.SteamId64 == steamPending.playerID.steamID.m_SteamID))
+            {
+                var find = Instance.Configuration.Instance.Players.Find(y => y.SteamId64 == player.m_SteamID);
+                steamPending.playerID.characterName = find.CharacterName;
+
+                Logger.Log($"Player \"{steamPending.playerID.playerName}\" has been renamed to \"{find.CharacterName}\".");
+            }
+            else return;
+        }
+    }
+}
---
+++ src/PSForcedNames/PSForcedNamesConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using Rocket.API;
+
+namespace PSForcedNames
+{
+    public class PSForcedNamesConfiguration : IRocketPluginConfiguration
+    {
+        public List<Player> Players;
+        
+        public bool Enabled;
+        
+        public void LoadDefaults()
+        {
+            Enabled = true;
+
+            Players = new List<Player>
+            {
+                new Player {SteamId64 = 76561198132469161, CharacterName = "God Himself"}
+            };
+        }
+    }
+
+    public class Player
+    {
+        [XmlAttribute] public ulong SteamId64;
+        [XmlAttribute] public string CharacterName;
+    }
+}
